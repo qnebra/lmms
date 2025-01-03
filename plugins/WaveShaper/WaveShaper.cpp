15d9f6181808a27fc02b847b0713f92bf37adee7
qnebra <qnebra@hotmail.com>
2025-01-03 23:09:06 +0100

    Revert "Merge remote-tracking branch 'szeli1/vectorGraph' into 03-01-2025"
    This reverts commit acc4bf41bfcc96dd4a25ba10487e5788ff795d70, reversing
    changes made to 69afaa358c8c2d9d67d88b01b2b59bbf7141f9cc.

--- plugins/WaveShaper/WaveShaper.cpp
+++ plugins/WaveShaper/WaveShaper.cpp
@@ -28,11 +28,8 @@
 #include "lmms_math.h"
 #include "embed.h"
 #include "interpolation.h"
-#include "VectorGraphModel.h"
 
 #include "plugin_export.h"
-
-#include <vector>
 
 namespace lmms
 {
@@ -78,9 +75,7 @@ Effect::ProcessStatus WaveShaperEffect::processImpl(SampleFrame* buf, const fpp_
 	const float w = wetLevel();
 	float input = m_wsControls.m_inputModel.value();
 	float output = m_wsControls.m_outputModel.value();
-	// getting the current graph samples (size: 200)
-	// getting the graph samples this often can cause lagg with bigger sample count (if automated or effected)
-	std::vector<float>* graphSamples = m_wsControls.getGraphSamples();
+	const float * samples = m_wsControls.m_wavegraphModel.samples();
 	const bool clip = m_wsControls.m_clipModel.value();
 
 	ValueBuffer *inputBuffer = m_wsControls.m_inputModel.valueBuffer();
@@ -117,17 +112,17 @@ Effect::ProcessStatus WaveShaperEffect::processImpl(SampleFrame* buf, const fpp_
 
 			if( lookup < 1 )
 			{
-				s[i] = frac * (*graphSamples)[0] * posneg;
+				s[i] = frac * samples[0] * posneg;
 			}
 			else if( lookup < 200 )
 			{
-				s[i] = linearInterpolate((*graphSamples)[lookup - 1],
-						(*graphSamples)[lookup], frac)
+				s[i] = linearInterpolate( samples[ lookup - 1 ],
+						samples[ lookup ], frac )
 						* posneg;
 			}
 			else
 			{
-				s[i] *= (*graphSamples)[199];
+				s[i] *= samples[199];
 			}
 		}
 
