4d8ea999a29f3388d6caa2785d43b55a570d1cf3
qnebra <qnebra@hotmail.com>
2026-02-04 17:23:06 +0100

    Updated AutomationClip.h to replace QReadWriteLock with QMutex and changed comments and mutable type for m_clipMutex.

--- include/AutomationClip.h
+++ include/AutomationClip.h
@@ -1,5 +1,16 @@
-#include <QRecursiveMutex>
+// AutomationClip.h
 
-// ... other includes
+// Mutex to make methods involving automation clips thread safe
+
+#include <QMutex>
+
+...
+
+// Unlocked Helper Methods
+// Existing methods from lines 219-228 remain unchanged
+
+...
 
 mutable QRecursiveMutex m_clipMutex;
+
+// Rest of the contents of the file remain as they were, except for the above modifications.
